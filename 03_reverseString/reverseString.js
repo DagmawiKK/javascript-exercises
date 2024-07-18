c160714d26f886dbc42c7915f553ae55d71fa22b
DagmawiKK <dagmawikassa1@gmail.com>
2024-07-18 12:58:46 +0300

    Solve by iterating the string in reverse

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let myStr = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        myStr = myStr + string[i];
+    };
+    return myStr;
 };
 
 // Do not edit below this line
