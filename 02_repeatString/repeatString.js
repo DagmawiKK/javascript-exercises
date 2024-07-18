286f5c1c5722d7be98adca08fc4574646e73c433
DagmawiKK <dagmawikassa1@gmail.com>
2024-07-18 12:54:06 +0300

    Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let myStr = "";
+    if (num < 0) {
+        return 'ERROR';
+    };
+    for (; num > 0; num--) {
+        myStr = myStr + string;
+    };
+    return myStr;
 };
 
 // Do not edit below this line
