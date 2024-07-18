665a3f35ec32ccdd92a6fc1e00e297a0c8fec967
DagmawiKK <dagmawikassa1@gmail.com>
2024-07-18 18:22:51 +0300

    Solve by creating a vardiac function and splicing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    const myArr = arguments[0]
+    for (let i = 1; i < arguments.length; i++) {
+        while (myArr.includes(arguments[i])) {
+            myArr.splice(myArr.indexOf(arguments[i]), 1);
+        }
+    }
+    return myArr;
 };
 
 // Do not edit below this line
