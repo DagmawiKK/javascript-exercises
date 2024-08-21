a580696e3980796e0767a12172f1a3a1c8506945
DagmawiKK <dagmawikassa1@gmail.com>
2024-08-21 12:02:03 +0300

    Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string
+                    .split("")
+                    .filter(char => /^[a-zA-Z0-9]$/.test(char))
+                    .map(char => char.toLowerCase());
+    return string.join("") == string.reverse().join(""); 
 };
 
 // Do not edit below this line
