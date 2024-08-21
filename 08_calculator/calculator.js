0ec90eec732c74570bd4d180be11c6b60e9e3224
DagmawiKK <dagmawikassa1@gmail.com>
2024-08-21 11:49:59 +0300

    Solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,38 @@
 const add = function() {
+  return arguments[0] + arguments[1];
 	
 };
-
 const subtract = function() {
-	
+  return arguments[0] - arguments[1];
 };
 
 const sum = function() {
-	
+	if (arguments[0].length == 0) {
+    return 0;
+  }
+  return arguments[0].reduce((total, current)  => total += current, 0);
 };
 
 const multiply = function() {
-
+	if (arguments[0].length == 0) {
+    return 0;
+  }
+  return arguments[0].reduce((total, current)  => total *= current, 1);
 };
 
 const power = function() {
-	
+	return arguments[0] ** arguments[1];
 };
 
 const factorial = function() {
+  if (arguments[0] == 1 || arguments[0] == 0) {
+    return 1;
+  }
+  let total = 1;
+  for(let i = 1; i <= arguments[0]; i++) {
+    total *= i;
+  }
+  return total;
 	
 };
 
